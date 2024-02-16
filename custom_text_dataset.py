56726cf754743894a4fe5a6f69f52cd051fe4e98
zhe rao <zheraoeminem@gmail.com>
2024-02-15 22:17:58 -0500

    added comment

--- custom_text_dataset.py
+++ custom_text_dataset.py
@@ -1,5 +1,13 @@
 import torch
 
+
+
+# the unsupervisedTokenizedTextDataset takes in tokenized text (could be at character or word leve, or other level of tokenization), 
+# desired block size (input sequence length), predict_size (model output length: default 1 - use input sequence to predict the next token)
+# the dataset will have length of len(full_txt) - (block_size+predict_size) +1
+# for getitem, when given index
+# txt[index], txt[index+1], ... txt[index+block_size - 1] will be input sequence with length block_size
+# txt[index+block_size] ,..., txt[index+block_size+predict_size-1] will be output sequence with length predict_size
 class unsupervisedTokenizedTextDataset(torch.utils.data.Dataset):
     def __init__(self,full_txt,block_size,predict_size=1):
         self.txt = full_txt
