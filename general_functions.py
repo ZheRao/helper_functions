0caa04db9605641e010d7ab84d90af701ec69c4e
unknown <zheraoeminem@gmail.com>
2024-03-05 14:03:50 -0500

    updated split function with special tokens in the text

--- general_functions.py
+++ general_functions.py
@@ -1,9 +1,14 @@
 class HelperFunctionsClass:
 
     # train_test_split function identifies one book from full text - multiple books, then return train and validation set for that book
-    def train_test_split(self,text,ending,ratio,starting_idx=0):
-        ending_idx = text.find(ending)
-        ending_idx += len(ending)
+    def train_test_split(self,text,ratio,starting_idx=0, book_start_token=False,ending=None,token="<|startofbook|>"):
+        if book_start_token:
+            ending_idx = text.find(token,10+starting_idx) -1
+            if ending_idx == -2: ending_idx = -1
+        else:
+            assert ending is not None, "Have to provide ending of the book unless there is book_start_token"
+            ending_idx = text.find(ending)
+            ending_idx += len(ending)
         book = text[starting_idx:ending_idx]
         n = int(ratio*len(book))
         n = self.locate_next_break(book,n)
